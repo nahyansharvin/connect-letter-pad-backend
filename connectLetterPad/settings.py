45948ca1a7b408239aa7335e326b710adf4cfacc
Nahyan Sharvin <nskonnalath@gmail.com>
2022-09-23 22:56:41 +0530

    Automatic configuration with django-heroku

--- connectLetterPad/settings.py
+++ connectLetterPad/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_31a(quh5l_#qa(dvg=k=l7g#hoaqoa)n@+g9-%=9(hbrtlebo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -128,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
