983d9595292c58398c4d1ec524bfaaadcf2cc0d2
Nahyan Sharvin <nskonnalath@gmail.com>
2022-10-02 14:28:15 +0530

    disable debug

--- connectLetterPad/settings.py
+++ connectLetterPad/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_31a(quh5l_#qa(dvg=k=l7g#hoaqoa)n@+g9-%=9(hbrtlebo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
